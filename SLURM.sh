02e42e3ec756cf847c1d7fe36df568d98c1dc431
unknown <herzbasti97@gmail.com>
2023-04-07 12:19:39 +0300

    slurm script

---
+++ SLURM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=CILRS            
+#SBATCH --nodes=1        
+#SBATCH --ntasks-per-node=1    
+#SBATCH --partition=ai       
+#SBATCH --qos=users        
+#SBATCH --account=users    
+#SBATCH --gres=gpu:tesla_t4:1    
+#SBATCH --time=7:0:0        
+#SBATCH --output=test-%j.out    
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sherz22@ku.edu.tr    
+module load python/3.6.1
+moddule load cuda/11.4
+module load 8.2.2/cuda-11.4     
+python cilrs_train.py
