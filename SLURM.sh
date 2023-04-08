70b3ddc5c7aacd6633487048f8bc2e1ae2bec12a
unknown <herzbasti97@gmail.com>
2023-04-08 19:09:20 +0300

    SLURM

--- SLURM.sh
+++ SLURM.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=CILRS            
-#SBATCH --ntasks-per-node=1
-#SBATCH --ntasks=4
+#SBATCH --nodes=1        
+#SBATCH --ntasks-per-node=4 
 #SBATCH --partition=long       
 #SBATCH --qos=users        
 #SBATCH --account=users    
