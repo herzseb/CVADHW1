23ac94c5a8cfc1183fbb801deb1af3e82b39f89e
unknown <herzbasti97@gmail.com>
2023-04-08 19:12:20 +0300

    SLURM

--- SLURM.sh
+++ SLURM.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=CILRS            
 #SBATCH --nodes=1        
 #SBATCH --ntasks-per-node=4 
-#SBATCH --partition=ai      
+#SBATCH --partition=shallowai  
 #SBATCH --qos=users        
 #SBATCH --account=users    
 #SBATCH --gres=gpu:tesla_t4:1    
