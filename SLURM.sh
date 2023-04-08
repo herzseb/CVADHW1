9fcad026fe9aa682f13877cad24a0baaf477a280
unknown <herzbasti97@gmail.com>
2023-04-08 19:07:39 +0300

    SLURM

--- SLURM.sh
+++ SLURM.sh
@@ -10,7 +10,6 @@
 #SBATCH --output=test-%j.out    
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sherz22@ku.edu.tr    
-nvidia-smi
 module load python/3.9.5
 module load cuda/11.4
 module load cudnn/8.2.2/cuda-11.4
