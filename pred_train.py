856d590cee7c3a0f16499c7c898e44a755c4af63
unknown <herzbasti97@gmail.com>
2023-04-15 15:15:51 +0300

    retain graph

--- pred_train.py
+++ pred_train.py
@@ -90,7 +90,7 @@ def train(model, loaders, optimizer, criterion_MAE, criterion_CE):
             loss = criterion_MAE(regs, regression_target)
             loss += criterion_CE(clas,
                                  torch.flatten(tl_state).to(dtype=torch.long))
-            loss.backward()
+            loss.backward(retain_graph=True)
             optimizer.step()
             running_loss += loss.item()
             it = it + 1
