f81282834cf8ff101752d4d00cf60aa38ee70aa0
unknown <herzbasti97@gmail.com>
2023-04-19 18:21:19 +0300

    clean up and fix vgg16

--- models/affordance_predictor.py
+++ models/affordance_predictor.py
@@ -9,10 +9,14 @@ class AffordancePredictor(nn.Module):
 
     def __init__(self):
         super(AffordancePredictor, self).__init__()
-        self.feature_extractor = models.vgg16(pretrained=True).features
+        self.vgg16_model = models.vgg16(pretrained=True)
+        self.vgg16_model.classifier = self.vgg16_model.classifier[:-1]
+        for param in self.vgg16_model.parameters():
+            param.requires_grad = False
+        
         self.queue_length = 10
         self.input_size = 512
-        self.hidden_size = 256
+        self.hidden_size = 128
         self.num_layers = 2
         self.drop_out = 0.2
         self.in_channel = 10
@@ -20,12 +24,12 @@ class AffordancePredictor(nn.Module):
         self.memory_size = 10
         self.do = nn.Dropout(self.drop_out)
         self.grus = nn.ModuleList()
-        self.fc_features =  nn.Linear(512 * 7 * 7, self.input_size)
+        self.fc_features =  nn.Linear(4096, self.input_size)
         for i in range(7):
             self.grus.append(torch.nn.GRU(input_size=self.input_size, hidden_size=self.hidden_size,
                                           num_layers=self.num_layers, batch_first=True, dropout=self.drop_out))
 
-        self.batchnorm = torch.nn.BatchNorm1d(10)
+        self.batchnorm = torch.nn.BatchNorm1d(self.memory_size)
         self.blocks = nn.ModuleList()
         for i in range(7):
             self.blocks.append(nn.Sequential(
@@ -48,8 +52,8 @@ class AffordancePredictor(nn.Module):
         )
 
     def forward(self, img, command, memory_stack):
-        features = self.feature_extractor(img)
-        features = features.view(-1, 512 * 7 * 7)
+        features = self.vgg16_model(img)
+        # features = features.view(-1, 512 * 7 * 7)
         features = torch.relu(features)
         features = self.fc_features(features)
         features = torch.unsqueeze(features, dim=1)
