385e422d33fc0dc589c95bafeeca1ef22bf12b95
unknown <herzbasti97@gmail.com>
2023-04-22 09:24:11 +0300

    very small

--- cilrs_train.py
+++ cilrs_train.py
@@ -153,7 +153,7 @@ def main():
     
 
     val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
-    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.0001) #5
+    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.001) #5
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
     train_losses = []
     val_losses = []
--- models/cilrs.py
+++ models/cilrs.py
@@ -18,14 +18,14 @@ class CILRS(nn.Module):
 
         self.dropout = 0.5  # 0.2
         self.hidden = 128
-        self.hidden_speed_prediction = 128
-        self.speed_features = 128
+        self.hidden_speed_prediction = 64
+        self.speed_features = 64
         self.feature_input = self.resnet_out + self.speed_features
         self.feature_output = 512
         self.speed_encoding = nn.Sequential(
             nn.Linear(1, self.speed_features),
-            nn.ReLU(),
-            nn.Linear(self.speed_features, self.speed_features),
+            # nn.ReLU(),
+            # nn.Linear(self.speed_features, self.speed_features),
             nn.ReLU(),
             nn.Linear(self.speed_features, self.speed_features),
         )
@@ -53,9 +53,9 @@ class CILRS(nn.Module):
                 nn.Linear(self.feature_output, self.hidden),
                 nn.BatchNorm1d(self.hidden),
                 nn.ReLU(),
-                nn.Linear(self.hidden, self.hidden),
-                nn.BatchNorm1d(self.hidden),
-                nn.ReLU(),
+                # nn.Linear(self.hidden, self.hidden),
+                # nn.BatchNorm1d(self.hidden),
+                # nn.ReLU(),
                 nn.Dropout(p=self.dropout),
                 nn.Linear(self.hidden, 3)
             ))
